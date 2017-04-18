31b8a4fdab0ac34af03bea5af2e39bf7b0d554b5
Chen, Zhuhui <chenzhuhui@idaoben.com>
2017-04-18 13:32:07 +0800

    Submitted codes about camera calibration.

---
+++ camera.py
@@ -0,0 +1,69 @@
+# -*- encoding: utf-8 -*-
+
+import matplotlib.pyplot as plot
+import matplotlib.image as mpimg
+import cv2
+import numpy as np
+import os
+
+
+class Camera(object):
+    def __init__(self):
+        self.cameraMatrix = None
+        self.distortionCoeff = None
+
+    def calibrate(self, ncols, nrows, cal_image_paths):
+        """Calibrate camera with the given chessboard images.
+        """
+        # generate object points array
+        objp = np.mgrid[0:ncols, 0:nrows].T.reshape(-1, 2)
+        objpoints = np.zeros((nrows * ncols, 3), np.float32)
+        objpoints[:, :2] = objp
+
+        objp = []
+        imgp = []
+        img_size = None
+        for img_path in cal_image_paths:
+            image = cv2.imread(img_path)
+            gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            if img_size is None:
+                img_size = gs.shape[-1::-1]
+
+            ret, corners = cv2.findChessboardCorners(gs, (ncols, nrows), None)
+            # if corners not found, skip the specific calibration image
+            if not ret:
+                continue
+            objp.append(objpoints)
+            imgp.append(corners)
+        # calibrate camera
+        assert img_size is not None, "Unable to determine image size."
+        assert len(objp) > 0, "No {} x {} corner pattern can be detected from the specifying calibration images."
+        assert len(imgp) > 0, "No {} x {} corner pattern can be detected from the specifying calibration images."
+        _, cmx, dist, _, _ = cv2.calibrateCamera(objp, imgp, img_size, None, None)
+        self.cameraMatrix = cmx
+        self.distortionCoeff = dist
+
+    def undistort(self, img):
+        """Undistort offered image.
+        """
+        assert self.cameraMatrix is not None, "Camera's not calibrated. Calibrate it first!"
+        assert self.distortionCoeff is not None, "Camera's not calibrated. Calibrate it first!"
+        return cv2.undistort(img, self.cameraMatrix, self.distortionCoeff)
+
+
+if __name__ == '__main__':
+    cal_images_dir = "./camera_cal"
+    camera = Camera()
+    camera.calibrate(9, 6, [cal_images_dir + "/" + p for p in os.listdir(cal_images_dir)])
+
+    test_img = "./camera_cal/calibration1.jpg"
+    img = cv2.imread(test_img)
+    undistorted = camera.undistort(img)
+
+    f, (ax1, ax2) = plot.subplots(1, 2, squeeze=True)
+    ax1.imshow(img)
+    ax1.set_title('Original image')
+    ax2.imshow(undistorted)
+    ax2.set_title('Undistorted image')
+    f.savefig('./output_images/camera_calibration.png')
