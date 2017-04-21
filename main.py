41ebb3846dd94e8c406345db5cd5b6d3289a171b
Chen, Zhuhui <chenzhuhui@idaoben.com>
2017-04-21 23:43:43 +0800

    Filled missing codes for a complete pipeline working on static image.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from camera import Camera
 from moviepy.editor import VideoFileClip
 from pipeline import LaneDetectionPipeline
+import numpy as np
 
 import os
 
@@ -11,28 +12,30 @@ camera.calibrate(9, 6, [cal_images_dir + "/" + p for p in os.listdir(cal_images_
 
 # setup perspective transform
 camera.setup_perspective_transform(
-    ((),
-     (),
-     (),
-     ()),
-    ((),
-     (),
-     (),
-     ()))
+    np.array(((597, 446),
+              (266, 670),
+              (1038, 670),
+              (682, 446)), dtype=np.float32),
+    np.array(((340, 90),
+              (340, 690),
+              (940, 690),
+              (940, 90)), dtype=np.float32))
 
 pipeline = LaneDetectionPipeline(camera)
 
-clip_output_path = "./output.mp4"
-input_clip = VideoFileClip('./harder_challenge_video.mp4')
-output_clip = input_clip.fl_image(lambda img: pipeline.process(img))
-output_clip.write_videofile(clip_output_path, audio=False)
+# clip_output_path = "./output.mp4"
+# input_clip = VideoFileClip('./challenge_video.mp4')
+# output_clip = input_clip.fl_image(lambda img: pipeline.process(img))
+# output_clip.write_videofile(clip_output_path, audio=False)
 
-# import cv2
-# import matplotlib.pyplot as plt
-#
-# output_path = "./test.jpg"
-# input_img = './test_images/test6.jpg'
-# img = cv2.imread(input_img)
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-# output = pipeline.process(img)
-# cv2.imwrite(output_path, cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
+import cv2
+import matplotlib.pyplot as plt
+
+output_path = "./test2.jpg"
+input_img = './test_images/test1.jpg'
+img = cv2.imread(input_img)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+output = pipeline.process(img)
+cv2.imwrite(output_path, cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
+plt.imshow(output)
+plt.show()
--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 from processor import Processor
-import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
 
 class LaneDetectionPipeline(object):
@@ -10,6 +11,18 @@ class LaneDetectionPipeline(object):
     def process(self, img):
         """Processing the input image, returns the processed image."""
         undistorted_img = self.camera.undistort(img)
-        binary = self.processor.extract(undistorted_img)
-        result = np.stack((binary, binary, binary), axis=2) * 255
+        extracted = self.processor.extract(undistorted_img)
+        birdview = self.camera.warp_perspective(extracted)
+
+        lane_centers = self.processor.apply_slide_window_search(birdview)
+        plt.imshow(birdview)
+        plt.show()
+
+        l_polyfit,_ = self.processor.fit_polynomial_for_lane(birdview, lane_centers.T[0])
+        r_polyfit,_ = self.processor.fit_polynomial_for_lane(birdview, lane_centers.T[1])
+
+        mask_img = self.processor.get_birdview_lane_mask_image(birdview, l_polyfit, r_polyfit)
+
+        unwarp_mask = self.camera.warp_inverse_perspective(mask_img)
+        result = cv2.addWeighted(img, 1, unwarp_mask, 0.3, 0)
         return result
